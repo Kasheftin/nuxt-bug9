52b357e38a285f25453f820f1af401a40d6742d0
Kasheftin <Kasheftin@gmail.com>
2019-01-16 14:40:32 +0200

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Main Page
+      </h1>
+      <div class="links">
+        <nuxt-link to="/sub/1" class="button--green">Go to 1</nuxt-link>
+        <nuxt-link to="/sub/2" class="button--green">Go to 2</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/sub/:id.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        {{ route.path }}
+      </h1>
+      <div class="links">
+        <nuxt-link to="/" class="button--green">Go to Main Page</nuxt-link>
+        <a class="button--green" @click="goto(1)">Go to 1</a>
+        <a class="button--green" @click="goto(2)">Go to 2</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    route () { return this.$route }
+  },
+  fetch () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, 1000)
+    })
+  },
+  methods: {
+    goto (n) {
+      this.$router.push({path: '/sub/' + n})
+      setTimeout(() => {
+        this.$router.push({query: {p1: 'v2'}})
+      }, 500)
+    }
+  }
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
